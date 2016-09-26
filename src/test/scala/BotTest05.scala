db13133d1821a83c3121a99017e4939ecb8d6d76
katsel <katsel@users.noreply.github.com>
2016-09-26 10:21:53 +0200

    Add Unit tests for all kinds of entities

--- src/test/scala/BotTest05.scala
+++ src/test/scala/BotTest05.scala
@@ -7,6 +7,13 @@ import org.scalatest.Matchers._
 class BotTest05 extends FunSuite {
   val bot = new ControlFunction((1,-1))
 
+  test("[ON React] The bot should not step on its own shoes") {
+    val serverInput: String = "React(generation=0,time=0,view=____M____,energy=100)"
+
+    val reply: String = bot.respond(serverInput)
+    reply.shouldNot(be("Move(direction=0:0)|Status(text=100)"))
+  }
+
   test("[ON React] The bot should move up and have its current energy state as status-message, if there are no impediments.") {
     val serverInput: String = "React(generation=0,time=0,view=_________,energy=100)"
 
@@ -28,18 +35,45 @@ class BotTest05 extends FunSuite {
     reply shouldBe empty
   }
 
-  test("""[ON React] The bot should move towards a zugar when it sees them""") {
+  test("""[ON React] The bot should move towards a Zugar when it sees them""") {
     val serverInput: String = "React(generation=0,time=0,view=________P,energy=100)"
 
     val reply: String = bot.respond(serverInput)
     reply.should(be("Move(direction=1:1)|Status(text=100)"))
   }
 
-  test("""[ON React] The bot should move towards a fluppet when it sees them""") {
+  test("""[ON React] The bot should move towards a Fluppet when it sees them""") {
     val serverInput: String = "React(generation=0,time=0,view=_______B_,energy=100)"
 
     val reply: String = bot.respond(serverInput)
     reply.should(be("Move(direction=0:1)|Status(text=100)"))
+  }
+
+  test("""[ON React] The bot should avoid walls when it sees them""") {
+    // walls everywhere but on the left and right
+    val serverInput: String = "React(generation=0,time=0,view=WWW_M_WWW,energy=100)"
+
+    val reply: String = bot.respond(serverInput)
+    // bot should move to the left or to the right
+    reply.should(be("Move(direction=-1:0)|Status(text=100)") or be("Move(direction=1:0)|Status(text=100)"))
+  }
+
+  test("""[ON React] The bot should avoid Toxiferae when it sees them""") {
+    // Toxiferae everywhere but on top
+    val serverInput: String = "React(generation=0,time=0,view=p_ppMpppp,energy=100)"
+
+    val reply: String = bot.respond(serverInput)
+    // bot should move to the top
+    reply.should(be("Move(direction=0:-1)|Status(text=100)"))
+  }
+
+  test("""[ON React] The bot should avoid Snorgs when it sees them""") {
+    // Snorgs everywhere but on the bottom left
+    val serverInput: String = "React(generation=0,time=0,view=bbbbMb_bb,energy=100)"
+
+    val reply: String = bot.respond(serverInput)
+    // bot should move to the bottom left
+    reply.should(be("Move(direction=-1:1)|Status(text=100)"))
   }
 
 }
